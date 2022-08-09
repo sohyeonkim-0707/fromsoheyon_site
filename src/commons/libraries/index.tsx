d4025fa5a22f1eac196c0c4b665a7fcb28e09455
sohyeon <hjthgus777@gmail.com>
2022-08-09 10:44:10 +0900

    endpoint edit / shop list fix

--- src/commons/libraries/index.tsx
+++ src/commons/libraries/index.tsx
@@ -11,7 +11,7 @@ const RESTORE_ACCESS_TOKEN = gql`
 export async function getAccessToken() {
   try {
     const graphQLClient = new GraphQLClient(
-      "https://backend06.codebootcamp.co.kr/graphql",
+      "https://backend06.codebootcamp.co.kr/graphql09",
       { credentials: "include" }
     );
     const result = await graphQLClient.request(RESTORE_ACCESS_TOKEN);
